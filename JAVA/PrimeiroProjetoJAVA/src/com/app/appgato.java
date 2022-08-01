59e22f889fd2b60f133b23565013d6294c9c407a
Wilame Vilanova <44853409+wilamevilanova@users.noreply.github.com>
2022-08-01 10:12:08 -0300

    Add files via upload

---
+++ JAVA/PrimeiroProjetoJAVA/src/com/app/appgato.java
@@ -0,0 +1,12 @@
+package com.app;
+
+import com.model.gato;
+
+public class appgato {
+	public static void main(String[] args) {
+		
+		gato Gato = new gato();
+		System.out.println(Gato);
+	}
+
+}
---
+++ JAVA/PrimeiroProjetoJAVA/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		System.out.println("Eh nois!!");
+		int a = 2;
+		int b = 3;
+		System.out.println(a+b);
+		System.out.println("A soma de A + B é: " + (a+b));
+	}
+
+}
